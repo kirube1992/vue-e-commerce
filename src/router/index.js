1f9915cb169b9c2a0ff3e86cca0da16c4e8b5179
kirubel <kirubelaynalem92@gmail.com>
2025-08-08 21:50:36 +0300

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: () => import('@/views/ProductPage.vue'),
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/views/CartView.vue'),
+    },
+  ],
+})
+
+export default router
